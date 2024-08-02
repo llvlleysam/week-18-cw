6807c919292e22777c468c934cd049ec26dcaacf
Bahar Mehrali Moghaddam <bahar.6304118@gmail.com>
2024-08-02 07:09:26 -0700

    third commit(queen's commit)

--- src/components/Sections/SideRight.jsx
+++ src/components/Sections/SideRight.jsx
@@ -2,16 +2,19 @@ import { useContext } from "react";
 import CartWallet from "../Common/Cart-Wallet";
 import Checkout from "../Common/Checkout";
 import { RootContext } from "../../Context/Context.context";
+import { useGetOrder } from "../../hooks/useGetProducts";
 
 export default function SideRight() {
     const{showCheckOut,setShowCheckOut}=useContext(RootContext)
+    const {data}= useGetOrder()
+    //console.log(data);
   return (
     <div className='col-span-1'>
       <div className='flex border-b-2 border-black justify-evenly py-4'>
         <p>Cart is empty now</p>
       </div>
       <div >
-        <CartWallet/>
+        {data?.map(order=><CartWallet order={order}/>)}
       </div>
       <div className="flex justify-around items-center">
         <span>Total : $ 999</span>
